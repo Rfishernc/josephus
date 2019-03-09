b992bf0570fa3bce6bd213de610199145b4e6a3f
RichFisher <rich.fishernc@gmail.com>
2019-03-09 11:01:33 -0600

    josephus you crafty devil you

--- Josephus/Program.cs
+++ Josephus/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Josephus
 {
@@ -6,7 +7,40 @@ namespace Josephus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number of people");
+            var numberOfPeople = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the killing interval.  You monster.");
+            var killingInterval = Int32.Parse(Console.ReadLine());
+            var people = new List<int>();
+            var killCounter = 0;
+            for (var i = 1; i <= numberOfPeople; i++)
+            {
+                people.Add(i);
+            }
+
+            void killTheNumbers()
+            {
+                    for (var i = 0; i < killingInterval; i++)
+                    {
+                        killCounter++;
+                        if (killCounter > people.Count)
+                        {
+                            killCounter = 1;
+                        }
+                    }
+                people.RemoveAt(killCounter - 1);
+                killCounter--;
+                if (people.Count > 1)
+                {
+                    killTheNumbers();
+                } else
+                {
+                    Console.WriteLine($"The last man (or woman) standing is number {people[0]}");
+                }
+            }
+
+            killTheNumbers();
+            Console.ReadLine();
         }
     }
 }
